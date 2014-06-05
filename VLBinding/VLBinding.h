14581cb59e7a36c0ca0a04e4dcfdaeb9b448a40e
Can YAMAN <can@yaman.me>
2014-06-05 10:42:00 +0300

    Initial Commit

---
+++ VLBinding/VLBinding.h
@@ -0,0 +1,13 @@
+//
+//  VLBinding.h
+//  VLBinding
+//
+//  Created by Can Yaman on 05/06/14.
+//  Copyright (c) 2014 Valensas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VLBinding : NSObject
+
+@end
---
+++ VLBinding/VLBinding.m
@@ -0,0 +1,13 @@
+//
+//  VLBinding.m
+//  VLBinding
+//
+//  Created by Can Yaman on 05/06/14.
+//  Copyright (c) 2014 Valensas. All rights reserved.
+//
+
+#import "VLBinding.h"
+
+@implementation VLBinding
+
+@end
